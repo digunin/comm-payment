66a177f496a554084f9c78232da662d395afb6ac
digunin <kandidad@gmail.com>
2024-04-12 11:55:15 +0300

    replace extra code from 'AddRecordButton' to 'createInitialValue' function

--- src/components/reportpage/AddRecordButton.tsx
+++ src/components/reportpage/AddRecordButton.tsx
@@ -5,41 +5,23 @@ import { AppDispatch } from "../../store";
 import { selectActualPrice } from "../../store/price/priceReducer";
 import { useLastRecord } from "../forms/useLastRecord";
 import { setMode } from "../../store/app-mode/appModeReducer";
+import { createInitialValue } from "../../utils";
 
 const AddRecordButton = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { latestRecord, newYear, newMonth } = useLastRecord();
   const price = useSelector(selectActualPrice);
 
-  const { cold, hot, electricity } = latestRecord;
+  const initialValue = createInitialValue(
+    newMonth,
+    newYear,
+    price,
+    latestRecord
+  );
 
   const onAddButtonClick = () => {
     dispatch(setMode("create-month-report"));
-    dispatch(
-      setInitialValues({
-        metersInputFields: {
-          cold: { value: cold.totalValue, error: null },
-          hot: { value: hot.totalValue, error: null },
-          electricity: { value: electricity.totalValue, error: null },
-        },
-        priceInputFields: {
-          cold: { value: price.cold, error: null },
-          hot: { value: price.hot, error: null },
-          electricity: { value: price.electricity, error: null },
-          waterWaste: { value: price.waterWaste, error: null },
-        },
-        monthAndYearInputFields: {
-          month: {
-            value: newMonth,
-            error: null,
-          },
-          year: {
-            value: newYear,
-            error: null,
-          },
-        },
-      })
-    );
+    dispatch(setInitialValues(initialValue));
   };
 
   return (
--- src/utils.tsx
+++ src/utils.tsx
@@ -3,6 +3,8 @@ import rootReducer from "./store/rootReducer";
 import { Provider } from "react-redux";
 import { render } from "@testing-library/react";
 import React from "react";
+import { MeterReadings, Months } from "./store/payment/paymentReducer.utils";
+import { Price } from "./store/price/priceReducer";
 
 export const renderWithProvider = (element: React.ReactElement) => {
   const store = configureStore({
@@ -13,3 +15,34 @@ export const renderWithProvider = (element: React.ReactElement) => {
   };
   return render(element, { wrapper });
 };
+
+export function createInitialValue(
+  month: Months,
+  year: number,
+  price: Price,
+  readings: MeterReadings
+) {
+  return {
+    metersInputFields: {
+      cold: { value: readings.cold.totalValue, error: null },
+      hot: { value: readings.hot.totalValue, error: null },
+      electricity: { value: readings.electricity.totalValue, error: null },
+    },
+    priceInputFields: {
+      cold: { value: price.cold, error: null },
+      hot: { value: price.hot, error: null },
+      electricity: { value: price.electricity, error: null },
+      waterWaste: { value: price.waterWaste, error: null },
+    },
+    monthAndYearInputFields: {
+      month: {
+        value: month,
+        error: null,
+      },
+      year: {
+        value: year,
+        error: null,
+      },
+    },
+  };
+}

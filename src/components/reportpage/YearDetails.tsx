30215b27d554081636d59ce9928d692ba064b5f2
digunin <kandidad@gmail.com>
2024-11-30 18:31:03 +0300

    demo block off, calendar fix

--- src/components/reportpage/YearDetails.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useSelected } from "../../hooks/report-page/useSelected";
-
-const YearDetails = () => {
-  const { selectedYear } = useSelected();
-  return <div className="details year-details">Выберите месяц</div>;
-};
-
-export default YearDetails;

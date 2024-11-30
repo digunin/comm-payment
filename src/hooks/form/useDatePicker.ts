0069b7a3c66bcf431bd02971312e27141eeec9ec
digunin <kandidad@gmail.com>
2024-11-30 18:23:23 +0300

    calendar fix

--- src/hooks/form/useDatePicker.ts
+++ src/hooks/form/useDatePicker.ts
@@ -7,7 +7,9 @@ export const useDatePicker = (
 ) => {
   const onPickDate = (event: React.ChangeEvent<HTMLInputElement>) => {
     const [year, month, _] = event.target.value.split("-");
-    onchange(Number(year), Number(month) - 1);
+    const today = new Date();
+    const [todayYear, todayMonth] = [today.getFullYear(), today.getMonth()];
+    onchange(Number(year) || todayYear, Number(month) - 1 || todayMonth);
   };
   const minDate =
     minYear === -1 ? "" : `${minYear}-${("0" + (minMonth + 1)).slice(-2)}-01`;
